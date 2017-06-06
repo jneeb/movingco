9ad6ee6a4a6f46e2aa6738077dc1447fa863b644
dubKelly <js.neeb1780@gmail.com>
2017-06-06 02:45:39 -0400

    -blog

--- functions/burger.js
+++ functions/burger.js
@@ -1,11 +1,37 @@
 $(document).ready(function () {
-	$("#burger").click(function () {
-		if ($("#burger").hasClass("isOpen")) {
-			$(this).removeClass("isOpen");
-			$("#mobileNav").removeClass("isOpen");
+	$(".burger").click(function () {
+		if ($(".burger").hasClass("open")) {
+			$(this).removeClass("open");
+			$(".mobileNav").removeClass("open");
 		} else {
-			$(this).addClass("isOpen");
-			$("#mobileNav").addClass("isOpen");
+			$(this).addClass("open");
+			$(".mobileNav").addClass("open");
 		}
 	});
 });
+var navToggle = document.getElementsByClassName("navToggle");
+var mNav = document.getElementsByClassName("mNav");
+for (var i = mNav.length - 1; i >= 0; i--) {
+	mNav[i].onclick = function() {
+		navToggle[0].classList.toggle("open");
+		navToggle[1].classList.toggle("open");
+	}
+}
+var scroll = document.getElementsByClassName("scroll");
+var docHeight = $(document).height();
+var vH = window.innerHeight;
+window.onscroll = function scrollY() {
+	var pageY = window.pageYOffset;
+	for (var i = scroll.length - 1; i >= 0; i--) {
+		if (pageY > 0 && pageY < (docHeight - vH)) {
+			scroll[0].classList.remove("scrolled");
+			scroll[1].classList.add("scrolled");
+			scroll[2].classList.add("scrolled");
+		}
+		else {
+			scroll[0].classList.add("scrolled");
+			scroll[1].classList.remove("scrolled");
+			scroll[2].classList.remove("scrolled");
+		}
+	}
+};
